49617db41243176666738212f65505f580d9c420
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-10-07 04:21:40 +1100

    SQL insert week 2

---
+++ w2/insersts.sql
@@ -0,0 +1,15 @@
+use DB_B;
+
+insert into Subjects 
+values ('BINS001', 'Business Information Systems'),
+('PROG001', 'Programming Fundamentals'), 
+('PROG002', 'Advanced Programming'), 
+('INDB001', 'Data Analytics'), 
+('IDSC001', 'Data Security'), 
+('IDSA001', 'Data Science'), 
+('INEF001', 'Networking Fundamentals'), 
+('IBRM001', 'Business Requirements Modelling'), 
+('IITC001', 'Introduction to Technical Communication');
+
+
+select * from Subjects;
