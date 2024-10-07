eee3df208baed94a5d43f21b4f64c9509ba3bd27
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-10-08 00:52:53 +1100

    changes

--- w2/otherQueries.sql
+++ w2/otherQueries.sql
@@ -14,3 +14,52 @@ select s.Stnum, s.Stname , u.Subnum, u.SubName, r.Mark from Students s
 select s.Stnum, s.Stname , u.Subnum, u.SubName, r.Mark from Students s, Results r, Subjects u     
     where u.Subname = 'Data Analytics' and s.Stnum = r.Stnum and u.Subnum = r.Subnum
     order by s.Stname;
+	
+	
+--Q3
+select s.Stnum, s.Stname, r.Subnum, r.Mark from Students s 
+    inner join Results r
+    on s.Stnum = r.Stnum
+    order by s.Stname desc;
+    
+
+-- another
+select s.Stnum, s.Stname, r.Subnum, r.Mark from Students s, Results r
+    where s.Stnum = r.Stnum
+    order by s.Stname desc;
+	
+	
+-- Q4
+select s.Stnum, s.Stname, s.Phone
+    --,r.Subnum, u.Subname, 
+    --r.Mark 
+    from Students s 
+    inner join Results r
+    on s.Stnum = r.Stnum
+    inner join Subjects u 
+    on u.Subnum = r.Subnum
+    where u.Subname = 'Data Security' and r.Mark >= 50;
+    
+
+-- another
+select s.Stnum, s.Stname, s.Phone, r.Subnum, u.Subname, r.Mark from Students s, Results r, Subjects u    
+    where s.Stnum = r.Stnum and u.Subnum = r.Subnum and u.Subname = 'Data Security' and r.Mark >= 50;
+	
+
+-- Q5
+select s.Stnum, s.Stname, s.Phone
+    --,r.Subnum, u.Subname, 
+    --r.Mark 
+    from Students s 
+    inner join Results r
+    on s.Stnum = r.Stnum
+    inner join Subjects u 
+    on u.Subnum = r.Subnum
+    where u.Subname = 'Advanced Programming' and r.Mark < 50;
+    
+
+-- another
+select s.Stnum, s.Stname, s.Phone, r.Subnum, u.Subname, r.Mark from Students s, Results r, Subjects u    
+    where s.Stnum = r.Stnum and u.Subnum = r.Subnum and u.Subname = 'Advanced Programming' and r.Mark < 50;
+
+
