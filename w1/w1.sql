866c07ba50e47ff095c9f41c2cbf702760ed2a9a
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-10-18 11:30:23 +1100

    w3 work

--- w1/w1.sql
+++ w1/w1.sql
@@ -8,12 +8,14 @@ create table Students (
 	Phone char(5)
 );
 
--- insert into Students values (1357, 'Jessica Steel', 'Manly', 54441);
--- insert into Students values (1245, 'Sandra Prasad', 'North Manly', 31117);
--- insert into Students values (1213, 'Jean Straus', 'North Bondi', 62236);
--- insert into Students values (1228, 'William Chen', 'Bondi Junction', 62225);
--- insert into Students values (1677, 'Albert Widjaya', 'South Bondi', 83336);
--- insert into Students values (6623, 'Amy Jackson', 'Manly', 13355);
--- insert into Students values (6644, 'Jackson Styles', 'Paddington', 12344);
+-- insert into Students values (1356, 'Jane Weebly', 'Manly', 54431);
+-- insert into Students values (1245, 'Sandra Ho', 'North Manly', 31317);
+-- insert into Students values (1214, 'Jean Paul', 'North Bondi', 63236);
+-- insert into Students values (1228, 'William Smith', 'Bondi Junction', 62625);
+-- insert into Students values (1677, 'Albert Chan', 'South Bondi', 83536);
+-- insert into Students values (6623, 'Emily Willis', 'Manly', 13355);
+-- insert into Students values (6645, 'Jackie Styles', 'Paddington', 12344);
 
 select * from Students;
+
+
--- w3/queries.sql
+++ w3/queries.sql
@@ -1,10 +1,34 @@
 
 -- 1 display all the subject codes and names completed by Emily Willis. Sort the results by subject name. 
 
-select r.StNum, s.StName, r.Subnum, u.Subname, r.Mark from Results r 
-inner join Students s on r.Stnum = s.Stnum
-inner join Subjects u on u.Subnum = r.Subnum
-where s.Stname = 'Emily Willis'
-order by u.Subname;
+select --r.StNum, s.StName, 
+    r.Subnum, u.Subname, r.Mark from Results r 
+    inner join Students s on r.Stnum = s.Stnum
+    inner join Subjects u on u.Subnum = r.Subnum
+    where s.Stname = 'Emily Willis' and r.mark is not null
+    order by u.Subname;
 
-select * from Students
+-- 2
+select s.Stnum, s.Stname, s.Suburb 
+    from Students s 
+    where s.Suburb like '%Bondi%'
+    order by s.Stname desc;
+	
+-- 3
+select r.StNum, r.Subnum, s.StName, r.Mark from Results r 
+    inner join Students s on r.Stnum = s.Stnum    
+    where r.mark is null
+    order by r.Subnum desc;
+	
+-- 4
+select r.StNum, s.StName, r.Mark,
+    r.Subnum, u.Subname from Results r 
+    inner join Students s on r.Stnum = s.Stnum
+    inner join Subjects u on u.Subnum = r.Subnum
+    where r.mark is not null and r.mark >= 50
+    order by u.Subname;
+	
+-- 5
+select s.Stnum, s.Stname, s.Suburb, s.Phone 
+    from Students s 
+    where s.Stname like 'J%';
