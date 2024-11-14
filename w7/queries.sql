fe080e742ac9e14f289f5f3038e5bb8744bffcfd
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-11-15 04:39:29 +1100

    w7 sql

--- w7/queries.sql
+++ w7/queries.sql
@@ -48,3 +48,41 @@ select s.SubNum, SubName, Mark, Suburb
 	and Mark > 50 
 	and Suburb = 'Manly' 
     order by 2, 3 desc;
+	
+-- q2.	lists the numbers and names of all students who have studied a subject. 
+-- Only include each number once in the report. 
+-- Sort the results by student number in ascending order and name in descending order.
+
+select distinct r.stNum, s.stname from Results r
+    inner join Students s on r.stnum = s.stnum
+    order by r.stNum, stname desc;
+	
+-- q3
+select s.stnum, stname, r.mark from Results r 
+    inner join Students s on r.stnum = s.stnum
+    where r.mark is null
+    order by s.stnum, stname desc;
+
+--q4
+select s.shopname, f.fname, f.fcolour, w.onhand from Flowers f 
+    inner join Whosells w on f.flowernum = w.flowernum
+    inner join Shops s on w.shopnum = s.shopnum
+    where (f.fcolour = 'yellow' and f.fname = 'lily')
+	and w.onhand > 0
+    order by s.shopname desc;
+
+-- q5
+select s.shopname, w.saleprice, (w.salePrice * 0.75) 'discountedPrice', w.flowernum, f.fname, w.onhand from Whosells w 
+    inner join Flowers f on f.flowernum = w.flowernum
+    inner join Shops s on s.shopnum = w.shopnum
+    where w.OnHand Between 50 and 100
+	and (w.salePrice * 0.75 > 8.0)
+	order by s.shopname, w.salePrice;
+
+select s.shopname, w.saleprice, (w.salePrice * 0.75) 'discountedPrice', w.flowernum, f.fname, w.onhand 
+    from Whosells w, Flowers f, Shops s 
+    where f.flowernum = w.flowernum
+	and s.shopnum = w.shopnum
+	and  w.OnHand Between 50 and 100
+	and (w.salePrice * 0.75 > 8.0)
+	order by s.shopname, w.salePrice;
