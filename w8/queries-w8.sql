469f6499768962eb6e01bb95d3833821f00456cb
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-11-21 04:52:00 +1100

    w8 queries

--- w8/queries-w8.sql
+++ w8/queries-w8.sql
@@ -52,16 +52,20 @@ select * from Profits;
 
 
 -- q5
-select f.fname, f.fcolour, w.onhand, s.shopName
+select f.fname, f.fcolour, sum(w.onhand) 'Total Stock'
     from Flowers f 
 	inner join Whosells w on f.flowerNum = w.flowerNum
 	inner join Shops s on w.shopNum = s.shopNum
 	where s.shopName != 'XYZ Florist'
-	and w.onhand between 100 and 400;
+	group by f.fname, f.fcolour
+	having sum(w.onhand) between 100 and 400
+	order by f.fname desc;
+	
 	
 -- q6
 -- •	SELECT, FROM, JOIN, ON, WHERE, GROUP BY, HAVING, ORDER BY
 select f.fcolour, f.fName, 
+    count(f.flowerNum) 'num of flowers',
     sum(w.onHand) 'total stock', 
     max(w.salePrice) 'Max sale price', 
     min(w.salePrice) 'Min sale price'
@@ -70,6 +74,7 @@ select f.fcolour, f.fName,
     where f.fcolour in ('yellow', 'orange')
     group by f.fcolour, f.fName
     having sum(w.onHand) > 50;
+    
 	
 --q7
 select s.shopname, s.suburb, f.fColour, f.Fname
@@ -79,5 +84,66 @@ select s.shopname, s.suburb, f.fColour, f.Fname
     where 
 	(f.fcolour in ('pink', 'orange') and f.fname like '%Rose%')
 	or 
-	(f.fcolour = 'yellow' and f.fname like '%Daisy%');
+	(f.fcolour = 'yellow' and f.fname like '%Daisy%')
+    order by s.shopname asc;
+		
+	
+--B
+--q1
+select f.flowernum, fname,fcolour, onhand  
+	from flowers f, whosells w 
+	where f.flowernum = w.flowernum  
+	and onhand> 100
+
+	
+-- q2
+select w.shopNum, f.fName, f.fColour, 
+    (w.salePrice * w.onhand) 'salePrice for all stock', 
+    (f.fcost * w.onhand) 'cost for all stock',
+    ((w.salePrice * w.onhand) - (f.fcost * w.onhand)) 'difference'
+    from 
+	Flowers f 
+    inner join 
+	Whosells w on f.flowerNum = w.flowerNum  
+    where 
+	f.fColour in ('yellow', 'white')
+	and  ((w.salePrice * w.onhand) - (f.fcost * w.onhand)) between 70 and 300
+    order by 
+	f.fName;
+
+-- q3
+select * from Shops;
+insert into Shops values (600,'Expert Florist','Manly South');
+select * from Shops;
+
+	
+select s.shopNum, s.shopName, s.suburb, w.flowerNum, w.onHand, w.salePrice
+    from 
+	Shops s
+    full outer join 
+	Whosells w 
+    on 
+	s.shopNum = w.shopNum
+    -- where shopname <> 'Floral Boutique'
+    order by s.shopNum, w.onHand desc;
+	
+-- q4
+select f.fName, f.fColour, sum(w.onHand) 'total stock in all shops'
+    from Flowers f
+    inner join Whosells w on f.flowerNum = w.flowerNum
+    where fName like '%rose%'
+    group by f.fName, f.fColour
+    having sum(w.onHand) > 50
+    order by f.fColour;
+	
+-- q5
+select f.fName, f.fColour, s.shopNum, s.shopName, s.suburb, sum(w.onhand) 'Total Qty'
+    from Flowers f
+    inner join WhoSells w 
+	on f.flowerNum = w.flowerNum
+    inner join Shops s
+	on w.shopNum = s.shopNum
+    group by f.fName, f.fColour, s.shopNum, s.shopName, s.suburb
+    having sum(w.onhand) > 75;
+    
 	
